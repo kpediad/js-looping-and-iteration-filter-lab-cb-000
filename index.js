bbf500a8de378014262aac50564297c43d6ab3cc
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-21 12:27:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(array, name) {
-  return array.filter(function (item) {return item.toLower​ === name.toLower;});
+  return array.filter(function (item) {return item​ === name;});
 }
