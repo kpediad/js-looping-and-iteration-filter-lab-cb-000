c1002ce14505ba81b64c9a5b38b6110f2d895914
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-21 12:37:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 function findMatching(array, name) {
   return array.filter(function (item) {return item.toLowerCase() === name.toLowerCase();});
 }
+function fuzzyMatch(array, letters) {
+  return array.filter(function (item) {return item.slice(letters.length) === letters;});
+}
