1844eb2f7d495b2b6a5e96ee7b28101fd135631a
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-21 12:33:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(array, name) {
-  return array.filter(function (item) {return item.toLowerCase() === name;});
+  return array.filter(function (item) {return item.toLowerCase() === name.toLowerCase();});
 }
